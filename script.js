7f9ec693c907b25581fdac1d636d53497c9ad5ff
GARRETT POWERS <GARRETT.POWERS@AGNET.TAMU.EDU>
2024-03-08 10:24:41 -0600

    Make pixels progressively darker

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-
-
 const makeGrid = (size) => {
-  document.querySelectorAll(".pix").forEach(e => e.remove());
+  document.querySelectorAll(".pix").forEach((e) => e.remove());
   for (let i = 0; i < size * size; i++) {
     const pix = document.createElement("div");
     pix.setAttribute("class", "pix");
-    pix.style.minWidth = 100/size + "%";
-    pix.addEventListener(
-      "mouseover",
-      () => (pix.style.backgroundColor = getRandomColor())
-    );
+      pix.style.filter = "brightness(1)";
+    pix.style.minWidth = 100 / size + "%";
+    pix.addEventListener("mouseover", () => {
+      pix.style.backgroundColor = getRandomColor();
+      const regExp = /\(([^)]+)\)/;
+      const currentFilterVal = regExp.exec(pix.style.filter);
+      pix.style.filter = `brightness(${currentFilterVal[1] - 0.1})`;
+    });
     grid.appendChild(pix);
     console.log("adding div");
   }
 };
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
+  var letters = "0123456789ABCDEF";
+  var color = "#";
   for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
@@ -36,12 +37,12 @@ sketchpad.appendChild(controls);
 
 // Make reset button
 const reset = document.createElement("button");
-reset.setAttribute("class", "button")
+reset.setAttribute("class", "button");
 reset.textContent = "RESET";
 reset.addEventListener("click", () => {
   let newSize = prompt();
   makeGrid(newSize);
-})
+});
 controls.appendChild(reset);
 
 makeGrid(16);
