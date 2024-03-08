fae093e36a9045d8d3c38c853d479ae6282f2c3a
garrett_powers <garrett.powers@agnet.tamu.edu>
2024-03-07 20:52:25 -0600

    Add button

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const sketchpad = document.querySelector(".sketchpad");
 
-// var fragment = document.createDocumentFragment();
+
 const makeGrid = (size) => {
   for (let i = 0; i < size * size; i++) {
     const pix = document.createElement("div");
@@ -9,10 +8,25 @@ const makeGrid = (size) => {
       "mouseover",
       () => (pix.style.backgroundColor = "black")
     );
-    sketchpad.appendChild(pix);
+    grid.appendChild(pix);
     console.log("adding div");
   }
 };
 
+
+const sketchpad = document.querySelector(".sketchpad");
+const grid = document.createElement("div");
+grid.setAttribute("class", "grid");
+sketchpad.appendChild(grid);
+
+// Make control panel for buttons and stuff
+const controls = document.createElement("div");
+controls.setAttribute("class", "controls");
+sketchpad.appendChild(controls);
+
+// Make reset button
+const reset = document.createElement("button");
+reset.textContent = "RESET";
+controls.appendChild(reset);
+
 makeGrid(16);
-// sketchpad.appendChild(fragment);
